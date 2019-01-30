1b43b73f2baa57a58d13de7ca7470567e3fc3f12
brandons209 <brandons209@aol.com>
2019-01-30 02:06:04 -0500

    build final weight_transfer code

--- models_src/weight_transfer.py
+++ models_src/weight_transfer.py
@@ -8,16 +8,32 @@ Transfers weights from one model to another by passing data into both models and
 Labels are binary, 1 if the input data was the same into each network and 0 otherwise.
 avg_dim is dimension created from averaging outputs of each model.
 """
-def Weight_Transfer(avg_dim=1024):
+def weight_transfer_fc(avg_dim):
+    inp_dim = avg_dim * 2
     model = nn.Sequential(OrderedDict([
-        ('fc0', nn.Linear(avg_dim*2, avg_dim*2)),
+        ('fc0', nn.Linear(inp_dim, inp_dim)),
         ('relu0', nn.ReLU(inplace=True)),
-        ('fc1', nn.Linear(avg_dim*2, 512)),
+        ('fc1', nn.Linear(inp_dim, inp_dim/4)),
         ('relu1', nn.ReLU(inplace=True)),
-        ('fc2', nn.Linear(512, 128)),
+        ('fc2', nn.Linear(inp_dim/4, inp_dim/16)),
         ('relu2', nn.ReLU(inplace=True)),
-        ('fc3', nn.Linear(128, 1)),
+        ('fc3', nn.Linear(inp_dim/16, 1)),
         ('out', nn.Sigmoid())
     ]))
 
     return model
+
+
+class Weight_Transfer(nn.Module):
+    def __init(self, twoD_model, threeD_model, avg_dim=1024):
+        self.twoD_model = twoD_model
+        self.threeD_model = threeD_model
+        self.inp_dim = avg_dim * 2
+
+        for param in twoD_model.parameters():
+            param.requires_grad = False
+
+        def forward(self, x):
+            twoD_out = None
+            for frame in x:
+                pred = self.twoD_model(x)
